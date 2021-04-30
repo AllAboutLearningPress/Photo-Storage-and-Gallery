2cac0e3931f324ee099b664925ca18e2939a0bbb
Ashraful Firoz <ashraf.akon201@gmail.com>
2021-04-30 08:13:49 +0600

    added upload controller

---
+++ resources/js/Pages/Photo/Show.vue
@@ -0,0 +1,17 @@
+<template>
+    <h1>{{ photo.name }}</h1>
+    <img
+        :src="'/' + photo.name"
+        :alt="photo.name"
+        style="height: 300px; width: 300px"
+    />
+</template>
+
+<script>
+export default {
+    props: {
+        photo: Object,
+    },
+    methods: {},
+};
+</script>
