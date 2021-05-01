3ca8c3630eb5e8504b2934bd762cbbd6bd668b4a
Ashraful Firoz <ashraf.akon201@gmail.com>
2021-05-02 00:20:44 +0600

    fixed minor bug related to tagcontroller in web.php

--- app/Http/Controllers/TagController.php
+++ app/Http/Controllers/TagController.php
@@ -7,9 +7,91 @@ use Illuminate\Http\Request;
 
 class TagController extends Controller
 {
-    public function search($partial_tag)
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
     {
-        return Tag::where('name', 'LIKE', $partial_tag . '%')
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+
+    /**
+     * Search Tags by partial tag name. It will be
+     * used in the tags search box when adding tags to photos.
+     *
+     * @param string $partial_name
+     */
+    public function searchTags($partial_name)
+    {
+        return Tag::where('name', 'LIKE', $partial_name . '%')
             ->select('id', 'name', 'slug')->limit(5)->get();
     }
 }
