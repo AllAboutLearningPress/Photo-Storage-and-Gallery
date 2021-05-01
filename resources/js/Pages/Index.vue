a66ca8d7b12e74698ae2c2fa0eeb08985095e702
Ashraful Firoz <ashraf.akon201@gmail.com>
2021-05-02 01:11:06 +0600

    added master layout

--- resources/js/Pages/Index.vue
+++ resources/js/Pages/Index.vue
@@ -1,21 +1,26 @@
 <template>
-    <h1>photos</h1>
-    <inertia-link href="/upload">upload files</inertia-link>
-    <inertia-link
-        v-for="photo in photos"
-        :key="photo.id"
-        :href="route('photos.show', [photo.id, photo.name])"
-    >
-        <img
-            :src="photo.name"
-            :alt="photo.name"
-            style="height: 300px; width: 300px"
-        />
-    </inertia-link>
+    <main-layout>
+        <inertia-link
+            v-for="photo in photos"
+            :key="photo.id"
+            :href="route('photos.show', [photo.id, photo.name])"
+        >
+            <img
+                :src="photo.name"
+                :alt="photo.name"
+                style="height: 300px; width: 300px"
+            />
+        </inertia-link>
+    </main-layout>
 </template>
 
 <script>
+import MainLayout from "../Layouts/MainLayout.vue";
+
 export default {
+    components: {
+        MainLayout,
+    },
     props: {
         photos: Array,
     },
