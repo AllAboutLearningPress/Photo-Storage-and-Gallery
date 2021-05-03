4cd53127b9702e758c150716160381e3a25a253e
Ashraful Firoz <ashraf.akon201@gmail.com>
2021-05-03 13:18:50 +0600

    added functions related to load more images on user scroll

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Models\Photo;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Storage;
 use Inertia\Inertia;
 
 class IndexController extends Controller
@@ -15,8 +17,30 @@ class IndexController extends Controller
      */
     public function index()
     {
+        $photos =  Photo::limit(2)->get();
         return Inertia::render('Index', [
-            'photos' => Photo::all()
+            'photos' => $this->add_temp_url($photos)
         ]);
+    }
+
+    public function fetch_more($requst)
+    {
+        $data = $requst->validate([
+            'offset' => "required|integer"
+        ]);
+
+        $photos =  Photo::offset($data['offset'])->limit(20)->get();
+        return $this->add_temp_url($photos);
+    }
+
+    public function add_temp_url($photos)
+    {
+        foreach ($photos as $photo) {
+            $photo->url = Storage::disk('s3')->temporaryUrl(
+                'full_size/' . $photo->file_name,
+                now()->addMinutes(5)
+            );
+        }
+        return $photos;
     }
 }
