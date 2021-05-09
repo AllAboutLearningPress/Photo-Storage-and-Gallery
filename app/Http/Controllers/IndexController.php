0e71f4c86e74d5af560daaa5df2d6537e5f62973
Ashraful Firoz <ashraf.akon201@gmail.com>
2021-05-10 03:54:11 +0600

    intgrating post upload

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -3,11 +3,15 @@
 namespace App\Http\Controllers;
 
 use App\Models\Photo;
+use Arr;
 use Cache;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Storage;
 use Inertia\Inertia;
+use Aws\Lambda\LambdaClient;
+use Aws\Credentials\Credentials;
+use PhpOption\None;
 
 class IndexController extends Controller
 {
@@ -18,11 +22,32 @@ class IndexController extends Controller
      */
     public function index()
     {
-        $photos =  Photo::limit(5)->get();
-        //dd($photos);
-        return Inertia::render('Index', [
-            'photos' => $this->add_temp_url($photos)
-        ]);
+
+
+        $credentials = new Credentials(config('services.ses.key'), config('services.ses.secret'));
+        $client = new LambdaClient(array(
+            'credentials' => $credentials,
+            'region' => config('services.ses.region'),
+            'version' => 'latest'
+        ));
+        $result = $client->invoke(array(
+            // FunctionName is required
+            'FunctionName' => 'arn:aws:lambda:us-east-1:728758055541:function:photo_post_upload',
+            'InvocationType' => 'RequestResponse',
+            'LogType' => 'None',
+            //'ClientContext' => 'string',
+            'Payload' => json_encode(array(
+                'photo' => '012c84847602494063e62b1a98e023c05064f333389d5465070d901a4c96408f.jpeg'
+            )),
+            //'Qualifier' => 'string',
+        ));
+        dd($result['Payload']->getContents());
+        // $photos =  Photo::limit(5)->get();
+        // //dd($photos);
+        // return Inertia::render('Index', [
+        //     'photos' => $this->add_temp_url($photos)
+        // ]);
+
     }
 
     /**
