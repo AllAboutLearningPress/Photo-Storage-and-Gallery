1b224aaa9f56e12c92a7c514b15720ffe11cdd82
Ashraful Firoz <ashraf.akon201@gmail.com>
2021-05-13 02:24:04 +0600

    converted index.html to inertia render

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -1,5 +1,5 @@
 <template>
-    <Header></Header>
+    <!-- <Header></Header> -->
     <slot></slot>
 </template>
 
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -8,16 +9,21 @@
         <title>{{ config('app.name', 'Laravel') }}</title>
 
         <!-- Fonts -->
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
+        {{-- <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap"> --}}
 
         <!-- Styles -->
-        <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+        {{-- <link rel="stylesheet" href="{{ mix('css/app.css') }}"> --}}
 
         <!-- Scripts -->
+        <script src="/js/vendor/pig.min.js"></script>
+        <script src="/js/frontend/bundle.js"></script>
         @routes
         <script src="{{ mix('js/app.js') }}" defer></script>
     </head>
-    <body class="font-sans antialiased">
+
+
+    <body class="is-uploading">
         @inertia
     </body>
+
 </html>
