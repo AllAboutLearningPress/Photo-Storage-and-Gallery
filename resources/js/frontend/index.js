fb45ab5a442810317e61a6d7ffe1cd84a7b7471d
Ashraful Firoz <ashraf.akon201@gmail.com>
2021-05-18 06:10:16 +0600

    working on search from fe

--- resources/js/frontend/index.js
+++ resources/js/frontend/index.js
@@ -3,7 +3,7 @@ import allowedMimeTypes from './util/allowedMimeTypes';
 import Sidebar from './components/Sidebar';
 import Search from './components/Search';
 //import Upload from './components/Upload';
-import GlobalDropTarget from './components/GlobalDropTarget';
+//import GlobalDropTarget from './components/GlobalDropTarget';
 
 // misc app-wide global code, can be included and forgotten about:
 import './components/global-misc';
@@ -13,12 +13,12 @@ import './components/tags';
 import './components/editable';
 
 // generic things
-const globalDropTarget = new GlobalDropTarget(allowedMimeTypes);
-const upload = new Upload(document.querySelector('.js-upload'));
+//const globalDropTarget = new GlobalDropTarget(allowedMimeTypes);
+//const upload = new Upload(document.querySelector('.js-upload'));
 
 // main page widgets
 //const contentSidebar = new Sidebar(document.querySelector('.js-content-sidebar'));
-const search = new Search(document.querySelector('.js-search'));
+//const search = new Search(document.querySelector('.js-search'));
 
 // image view widgets
 const imageDetailsSidebar = new Sidebar(document.querySelector('.js-image-details-sidebar'), {
