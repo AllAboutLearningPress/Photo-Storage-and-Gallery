8dd3ca58b1f63a8664856a8e50330bd39907ed5f
Ashraful Firoz <ashraf.akon201@gmail.com>
2021-06-06 12:08:47 +0600

    all progress bar now working with vue inject/provider

--- resources/js/Components/UploadProgressBar.vue
+++ resources/js/Components/UploadProgressBar.vue
@@ -55,7 +55,8 @@ export default {
             for (let i = 0; i < this.filesArray.length; i++) {
                 loaded += this.filesArray[i].loaded;
             }
-            return loaded;
+            //console.log("loaded now ", loaded);
+            return (loaded / this.total) * 100 + "%";
         },
     },
 
