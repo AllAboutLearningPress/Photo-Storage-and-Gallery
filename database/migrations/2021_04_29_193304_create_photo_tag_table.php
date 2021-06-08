9f7e064fec2da02ef1caa392e7029f1dbf89e035
Ashraful Firoz <ashraf.akon201@gmail.com>
2021-06-08 10:48:05 +0600

    updated photo_tag pivot table and tags being added successfully

--- database/migrations/2021_04_29_193304_create_photo_tag_table.php
+++ database/migrations/2021_04_29_193304_create_photo_tag_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePhotoTagsTable extends Migration
+class CreatePhotoTagTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,8 +13,10 @@ class CreatePhotoTagsTable extends Migration
      */
     public function up()
     {
-        Schema::create('photo_tags', function (Blueprint $table) {
+        Schema::create('photo_tag', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('photo_id')->constrained('photos');
+            $table->foreignId('tag_id')->constrained('tags');
             $table->timestamps();
         });
     }
@@ -26,6 +28,6 @@ class CreatePhotoTagsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('photo_tags');
+        Schema::dropIfExists('photo_tag');
     }
 }
