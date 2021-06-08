607725560b871978769d096e63414eed71305f9b
Ashraful Firoz <ashraf.akon201@gmail.com>
2021-06-09 01:26:57 +0600

    refactored upload.vue componenet

--- resources/js/Pages/Upload/Upload.vue
+++ resources/js/Pages/Upload/Upload.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <!-- <upload-toolbar></upload-toolbar> -->
-        <h2 v-if="filesArray.len" class="fw-light mb-5">
+        <h2 v-if="filesArray.length" class="fw-light mb-5">
             Uploading {{ filesArray.len }} pictures, please take a moment to add
             details, or keep using a site
             <button
@@ -43,7 +43,7 @@
                             </div>
 
                             <button
-                                v-on:click="removeFile($event, file.id)"
+                                v-on:click="cancelSingleUpload($event, file.id)"
                                 title="Remove file"
                                 type="button"
                                 class="
@@ -68,99 +68,10 @@
                                 ></div>
                             </div>
                             <div class="file__header">
-                                <div class="js-editable editable file__title">
-                                    <h3
-                                        v-on:click="editTitle($event, file.id)"
-                                        class="editable__content fs-4 fw-light"
-                                    >
-                                        <span class="js-editable__val">
-                                            {{ file.title }}
-                                        </span>
-                                        <button
-                                            type="button"
-                                            title="Edit"
-                                            class="
-                                                js-editable__trigger
-                                                editable__trigger
-                                                d-inline-flex
-                                                btn btn-subtle btn-lg
-                                            "
-                                            aria-label="Edit"
-                                        >
-                                            <svg
-                                                xmlns="http://www.w3.org/2000/svg"
-                                                width="20"
-                                                height="20"
-                                                fill="currentColor"
-                                                class="bi bi-pen"
-                                                viewBox="0 0 16 16"
-                                            >
-                                                <path
-                                                    d="m13.498.795.149-.149a1.207 1.207 0 1 1 1.707 1.708l-.149.148a1.5 1.5 0 0 1-.059 2.059L4.854 14.854a.5.5 0 0 1-.233.131l-4 1a.5.5 0 0 1-.606-.606l1-4a.5.5 0 0 1 .131-.232l9.642-9.642a.5.5 0 0 0-.642.056L6.854 4.854a.5.5 0 1 1-.708-.708L9.44.854A1.5 1.5 0 0 1 11.5.796a1.5 1.5 0 0 1 1.998-.001zm-.644.766a.5.5 0 0 0-.707 0L1.95 11.756l-.764 3.057 3.057-.764L14.44 3.854a.5.5 0 0 0 0-.708l-1.585-1.585z"
-                                                />
-                                            </svg>
-                                        </button>
-                                    </h3>
-                                    <div
-                                        class="js-editable__form editable__form"
-                                        action="#"
-                                    >
-                                        <textarea
-                                            disabled
-                                            placeholder="Write here"
-                                            class="
-                                                js-editable__area
-                                                editable__area
-                                                fs-4
-                                                fw-light
-                                                form-control
-                                                form-control-lg
-                                                form-control-plaintext
-                                            "
-                                            v-model="file.title"
-                                            :id="'title' + file.id"
-                                        ></textarea>
-                                        <button
-                                            v-on:click="
-                                                saveTitle($event, file.id)
-                                            "
-                                            class="
-                                                js-editable__confirm
-                                                btn btn-outline-secondary
-                                            "
-                                        >
-                                            Ok
-                                        </button>
-                                    </div>
-                                </div>
-                                <div class="file__notes">
-                                    <a
-                                        class="
-                                            file__note
-                                            btn btn-outline-primary
-                                        "
-                                        href="#"
-                                        >Choose a family?</a
-                                    >
-                                    <a
-                                        v-if="file.hasDuplicate"
-                                        class="
-                                            file__note
-                                            btn btn-outline-warning
-                                        "
-                                        href="#"
-                                        >Compare duplicate</a
-                                    >
-                                    <a
-                                        v-else
-                                        class="
-                                            file__note
-                                            btn btn-outline-warning
-                                        "
-                                        href="#"
-                                        >Checking for duplicate</a
-                                    >
-                                </div>
+                                <file-title></file-title>
+                                <file-notes
+                                    :hasDuplicate="file.hasDuplicateddssss"
+                                ></file-notes>
                             </div>
                             <div class="js-tags tags">
                                 <div
@@ -266,9 +177,11 @@ import UploadToolbar from "./Components/UploadToolbar.vue";
 import { notify } from "@/util.js";
 import { addEventListener } from "@/frontend/util/utils.js";
 import { inject } from "@vue/runtime-core";
-import axios from "axios";
+import FileNotes from "./Components/ FileNotes.vue";
+import FileTitle from "./Components/FileTitle.vue";
+
 export default {
-    components: { UploadToolbar },
+    components: { UploadToolbar, FileNotes, FileTitle },
     layout: MainLayout,
     setup() {
         const pushToFilesArray = inject("pushToFilesArray");
@@ -324,6 +237,10 @@ export default {
     },
 
     methods: {
+        animateUploadBar(e) {
+            console.log(e);
+            window.requestAnimationFrame(() => this.updateUploadBar(e));
+        },
         /**Function to process a focusout event */
         focusOut(e) {
             console.log(e);
@@ -398,7 +315,7 @@ export default {
         },
 
         /**
-         * gets called when add button is pressed on tag
+         * Gets called when add button is pressed on tag
          * @param {Event} e - Add button click event
          * @param {Int} fileId - the id of the file that the tag is being added to
          */
@@ -440,14 +357,18 @@ export default {
                             name: tagInput.value,
                             id: tagId,
                         });
-                        this.saveTag(this.filesArray[k].id, tagId);
+                        this.sendAddTagReq(this.filesArray[k].id, tagId);
                         console.log(this.filesArray[k].tags);
                         break;
                     }
                 }
             }
         },
-        saveTag(fileId, tagId) {
+        /** Sends "add tag" request to server
+         * @param {Number} fileId - The id of the file where tag should be added
+         * @param {Number} tagId - The if of the tag to add
+         */
+        sendAddTagReq(fileId, tagId) {
             axios
                 .post(route("uploads.add_tag"), {
                     fileId: fileId,
@@ -462,7 +383,11 @@ export default {
                     notify("Tag Added successfully");
                 });
         },
-        deleteTag(fileId, tagId) {
+        /** Sends "delete tag" request to server
+         * @param {Number} - The id of the file to remove tag from
+         * @param {Number} - The id of tag to remove
+         */
+        sendDeleteTagReq(fileId, tagId) {
             axios
                 .post(route("uploads.remove_tag"), {
                     fileId: fileId,
@@ -485,7 +410,7 @@ export default {
             for (let i = 0; i < this.filesArray.length; i++) {
                 if (this.filesArray[i].id == fileId) {
                     console.log(this.filesArray[i]);
-                    this.deleteTag(
+                    this.sendDeleteTagReq(
                         this.filesArray[i].id,
                         this.filesArray[i].tags[tagIndex].id
                     );
@@ -496,49 +421,43 @@ export default {
         /**Cancels the full upload */
         cancelUpload() {
             console.log("upload cancelled");
+            let ids = [];
+            for (let i = 0; i < this.filesArray.length; i++) {
+                ids.push(this.filesArray[i].id);
+            }
+            this.sendCancelUploadReq(ids, "Upload Cancelled");
+            this.$inertia.get(route("home"));
+            this.filesArray = [];
         },
 
-        animateUploadBar(e) {
-            console.log(e);
-            window.requestAnimationFrame(() => this.updateUploadBar(e));
-        },
-        /**Function for updating individual progressbar */
-        // updateUploadBar(e) {
-        //     for (let i = 0; i < this.filesArray.length; i++) {
-        //         if (this.filesArray[i].id == e.detail.fileId) {
-        //             console.log("file found in uploading files");
-        //             this.filesArray[i].width =
-        //                 (e.detail.loaded / e.detail.total) * 100 + "%";
-        //             break;
-        //         }
-        //     }
-        // },
-        removeFile(e, fileId) {
+        cancelSingleUpload(e, fileId) {
             for (let i = 0; i < this.filesArray.length; i++) {
                 if (this.filesArray[i].id == fileId) {
                     // file found
+                    // cancelling the ongoing upload
                     this.filesArray[i].cancelToken.cancel();
+                    let title = this.filesArray[i].title;
 
-                    axios
-                        .post(route("uploads.cancel_upload"), [
-                            {
-                                id: this.filesArray[i].id,
-                            },
-                        ])
-                        .then((resp) => {
-                            // let user know that this file is cancelled
-                            notify(
-                                "Upload cancelled: " + this.filesArray[i].title
-                            );
-                        })
-                        .catch((err) => {
-                            console.error(err);
-                        });
-
+                    // removing the file from filesArray
+                    this.filesArray.splice(i, 1);
+                    this.sendCancelUploadReq([fileId], "File removed");
                     // removing the entry from filesArray.
                     //this.filesArray.splice(i, 1);
                 }
             }
+        },
+        sendCancelUploadReq(ids, notificationText) {
+            // sending request to remove the file entry
+            axios
+                .post(route("uploads.cancel_upload"), ids)
+                .then((resp) => {
+                    // let user know that this file is cancelled
+
+                    notify(notificationText);
+                })
+                .catch((err) => {
+                    console.error(err);
+                });
         },
     },
     beforeUnmount() {
