a298bef57402789306eb9baf6d7bc843680d7aeb
Ashraful Firoz <ashraf.akon201@gmail.com>
2021-06-11 16:05:50 +0600

    created button componenets for photoview page

--- resources/js/Pages/PhotoView/PhotoView.vue
+++ resources/js/Pages/PhotoView/PhotoView.vue
@@ -58,61 +58,9 @@
                             <span class="toolbar__specific-actions-more">
                                 <share-button></share-button>
                                 <download-button></download-button>
-                                <button
-                                    title="Delete image"
-                                    type="button"
-                                    class="js-image-delete btn-subtle btn"
-                                >
-                                    <svg
-                                        xmlns="http://www.w3.org/2000/svg"
-                                        width="24"
-                                        height="24"
-                                        fill="currentColor"
-                                        class="bi bi-trash"
-                                        viewBox="0 0 16 16"
-                                    >
-                                        <path
-                                            d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"
-                                        ></path>
-                                        <path
-                                            fill-rule="evenodd"
-                                            d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"
-                                        ></path>
-                                    </svg>
-                                    <span class="visually-hidden"
-                                        >Download</span
-                                    >
-                                </button>
+                                <delete-button></delete-button>
                             </span>
-                            <button
-                                title="Show details"
-                                type="button"
-                                class="
-                                    js-sidebar-toggle
-                                    image-view__show-details
-                                    btn-subtle btn
-                                "
-                                data-sidebar="#image-details-sidebar"
-                            >
-                                <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    width="24"
-                                    height="24"
-                                    fill="currentColor"
-                                    class="bi bi-info-circle"
-                                    viewBox="0 0 16 16"
-                                >
-                                    <path
-                                        d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"
-                                    />
-                                    <path
-                                        d="m8.93 6.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"
-                                    />
-                                </svg>
-                                <span class="visually-hidden"
-                                    >Show details</span
-                                >
-                            </button>
+                            <show-details-button></show-details-button>
                         </div>
                         <div class="toolbar__main-actions">
                             <label
@@ -164,7 +112,7 @@
                         class="image-view__img"
                         width="800"
                         height="800"
-                        src="//placekitten.com/800/800"
+                        :src="'/storage/full_size/' + photo.file_name"
                         alt=""
                     />
                 </div>
@@ -214,12 +162,21 @@ import MainLayout from "@/Layouts/MainLayout.vue";
 import BackButton from "./Componenets/BackButton.vue";
 import HomeButton from "./Componenets/HomeButton.vue";
 import DownloadButton from "./Componenets/DownloadButton.vue";
-import ShareButton from './Componenets/ShareButton.vue';
+import ShareButton from "./Componenets/ShareButton.vue";
+import DeleteButton from "./Componenets/DeleteButton.vue";
+import ShowDetailsButton from "./Componenets/ShowDetailsButton.vue";
 export default {
-    components: { BackButton, HomeButton },
-    layout: M,
-  ,
-        ShareButton  DownloadButtonainLayout,
+    props: ["photo"],
+    components: {
+        BackButton,
+        HomeButton,
+        ShowDetailsButton,
+        DeleteButton,
+        ShareButton,
+        DownloadButton,
+    },
+    layout: MainLayout,
+
     setup() {
         const toggleHeader = inject("toggleHeader");
         const showHeader = inject("showHeader");
@@ -229,9 +186,15 @@ export default {
         console.log(showHeader);
         return { showHeader, toggleHeader };
     },
+    data() {
+        return {
+            imgUrl: "//placekitten.com/800/800",
+        };
+    },
     mounted() {
         // this.photoView.value = true;
         //this.toggleHeader(false);
+        console.log(this.photo);
     },
 
     beforeMount() {
