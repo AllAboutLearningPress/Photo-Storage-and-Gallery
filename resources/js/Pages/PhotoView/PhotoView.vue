a9e6d1bd164ecedbd883191905e9551bbcabc2d4
Ashraful Firoz <ashraf.akon201@gmail.com>
2021-06-11 13:00:11 +0600

    added download, home and share button in photoview

--- resources/js/Pages/PhotoView/PhotoView.vue
+++ resources/js/Pages/PhotoView/PhotoView.vue
@@ -5,30 +5,7 @@
                 <div class="image-view__toolbar toolbar">
                     <div class="toolbar__main">
                         <back-button></back-button>
-                        <a
-                            title="Go home"
-                            href="#"
-                            class="js-go-home btn-subtle btn"
-                        >
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                width="24"
-                                height="24"
-                                fill="currentColor"
-                                class="bi bi-house"
-                                viewBox="0 0 16 16"
-                            >
-                                <path
-                                    fill-rule="evenodd"
-                                    d="M2 13.5V7h1v6.5a.5.5 0 0 0 .5.5h9a.5.5 0 0 0 .5-.5V7h1v6.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 13.5zm11-11V6l-2-2V2.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5z"
-                                />
-                                <path
-                                    fill-rule="evenodd"
-                                    d="M7.293 1.5a1 1 0 0 1 1.414 0l6.647 6.646a.5.5 0 0 1-.708.708L8 2.207 1.354 8.854a.5.5 0 1 1-.708-.708L7.293 1.5z"
-                                />
-                            </svg>
-                            <span class="visually-hidden">Go home</span>
-                        </a>
+                        <home-button></home-button>
                     </div>
                     <div class="toolbar__aside">
                         <div class="toolbar__specific-actions">
@@ -79,49 +56,8 @@
                                 >
                             </label>
                             <span class="toolbar__specific-actions-more">
-                                <button
-                                    title="Share image"
-                                    type="button"
-                                    class="js-image-share btn-subtle btn"
-                                >
-                                    <svg
-                                        xmlns="http://www.w3.org/2000/svg"
-                                        width="24"
-                                        height="24"
-                                        fill="currentColor"
-                                        class="bi bi-share"
-                                        viewBox="0 0 16 16"
-                                    >
-                                        <path
-                                            d="M13.5 1a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3zM11 2.5a2.5 2.5 0 1 1 .603 1.628l-6.718 3.12a2.499 2.499 0 0 1 0 1.504l6.718 3.12a2.5 2.5 0 1 1-.488.876l-6.718-3.12a2.5 2.5 0 1 1 0-3.256l6.718-3.12A2.5 2.5 0 0 1 11 2.5zm-8.5 4a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3zm11 5.5a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3z"
-                                        ></path>
-                                    </svg>
-                                    <span class="visually-hidden">Share</span>
-                                </button>
-                                <button
-                                    title="Download image"
-                                    type="button"
-                                    class="js-image-download btn-subtle btn"
-                                >
-                                    <svg
-                                        xmlns="http://www.w3.org/2000/svg"
-                                        width="24"
-                                        height="24"
-                                        fill="currentColor"
-                                        class="bi bi-cloud-download"
-                                        viewBox="0 0 16 16"
-                                    >
-                                        <path
-                                            d="M4.406 1.342A5.53 5.53 0 0 1 8 0c2.69 0 4.923 2 5.166 4.579C14.758 4.804 16 6.137 16 7.773 16 9.569 14.502 11 12.687 11H10a.5.5 0 0 1 0-1h2.688C13.979 10 15 8.988 15 7.773c0-1.216-1.02-2.228-2.313-2.228h-.5v-.5C12.188 2.825 10.328 1 8 1a4.53 4.53 0 0 0-2.941 1.1c-.757.652-1.153 1.438-1.153 2.055v.448l-.445.049C2.064 4.805 1 5.952 1 7.318 1 8.785 2.23 10 3.781 10H6a.5.5 0 0 1 0 1H3.781C1.708 11 0 9.366 0 7.318c0-1.763 1.266-3.223 2.942-3.593.143-.863.698-1.723 1.464-2.383z"
-                                        ></path>
-                                        <path
-                                            d="M7.646 15.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 14.293V5.5a.5.5 0 0 0-1 0v8.793l-2.146-2.147a.5.5 0 0 0-.708.708l3 3z"
-                                        ></path>
-                                    </svg>
-                                    <span class="visually-hidden"
-                                        >Download</span
-                                    >
-                                </button>
+                                <share-button></share-button>
+                                <download-button></download-button>
                                 <button
                                     title="Delete image"
                                     type="button"
@@ -276,9 +212,14 @@
 import { inject } from "@vue/runtime-core";
 import MainLayout from "@/Layouts/MainLayout.vue";
 import BackButton from "./Componenets/BackButton.vue";
+import HomeButton from "./Componenets/HomeButton.vue";
+import DownloadButton from "./Componenets/DownloadButton.vue";
+import ShareButton from './Componenets/ShareButton.vue';
 export default {
-    components: { BackButton },
-    layout: MainLayout,
+    components: { BackButton, HomeButton },
+    layout: M,
+  ,
+        ShareButton  DownloadButtonainLayout,
     setup() {
         const toggleHeader = inject("toggleHeader");
         const showHeader = inject("showHeader");
