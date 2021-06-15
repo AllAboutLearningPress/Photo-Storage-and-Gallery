48ee291f611332a3ad5e466d449aae2b81f842f7
Ashraful Firoz <ashraf.akon201@gmail.com>
2021-06-15 10:10:26 +0600

    added flash message support for inertia

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -37,7 +37,9 @@ class HandleInertiaRequests extends Middleware
     public function share(Request $request)
     {
         return array_merge(parent::share($request), [
-            //
+            'flash' => [
+                'message' => fn () => $request->session()->get('message')
+            ],
         ]);
     }
 }
