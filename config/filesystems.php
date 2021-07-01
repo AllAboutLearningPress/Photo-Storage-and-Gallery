72d9f339d7aa3825b39d7ee0368c912b8043522d
Ashraful Firoz <ashraf.akon201@gmail.com>
2021-07-02 03:08:12 +0600

    updated filesystem to use metadata credentials

--- config/filesystems.php
+++ config/filesystems.php
@@ -44,8 +44,8 @@ return [
 
         's3_fullsize' => [
             'driver' => 's3',
-            'key' => env('AWS_ACCESS_KEY_ID'),
-            'secret' => env('AWS_SECRET_ACCESS_KEY'),
+            'key' => '',
+            'secret' => '',
             'region' => env('AWS_DEFAULT_REGION'),
             'bucket' => env('FULLSIZE_BUCKET'),
             // 'url' => env('AWS_URL'),
