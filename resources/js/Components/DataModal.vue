e5b6149cd9163ea202c2d898be974a78e19d97f7
Ashraful Firoz <ashraf.akon201@gmail.com>
2021-07-18 13:12:26 +0600

    added share model refactored delete model to a componenet

---
+++ resources/js/Components/DataModal.vue
@@ -0,0 +1,46 @@
+<template>
+    <div
+        class="modal fade"
+        :id="modalId"
+        aria-hidden="true"
+        aria-labelledby
+        tabindex="-1"
+        data-backdrop="false"
+    >
+        <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="deleteModalToggleLabel">
+                        {{ title }}
+                    </h5>
+                    <button
+                        type="button"
+                        class="btn-close"
+                        data-bs-dismiss="modal"
+                        aria-label="Close"
+                    ></button>
+                </div>
+                <div class="modal-body">
+                    <slot name="body"></slot>
+                </div>
+                <div class="modal-footer">
+                    <button
+                        class="btn btn-primary"
+                        :data-bs-target="'#' + modalId"
+                        data-bs-toggle="modal"
+                        data-bs-dismiss="modal"
+                    >
+                        Cancel
+                    </button>
+                    <slot name="action_button"> </slot>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ["title", "deleted", "modalId"],
+};
+</script>
--- resources/js/Pages/PhotoView/Componenets/ShareButton.vue
+++ resources/js/Pages/PhotoView/Componenets/ShareButton.vue
@@ -1,6 +1,5 @@
 <template>
     <button
-        v-on:click="genShareableUrl"
         title="Share image"
         type="button"
         class="js-image-share btn-subtle btn"
@@ -25,19 +24,6 @@
 import { notify } from "@/util.js";
 export default {
     props: ["photo_id"],
-    methods: {
-        genShareableUrl() {
-            axios
-                .post(route("share.create"), { photo_id: this.photo_id })
-                .then((resp) => {
-                    console.log(resp);
-                    window.open(resp.data);
-                })
-                .catch((err) => {
-                    console.error(err);
-                    notify("Something went wrong. Please try again", "danger");
-                });
-        },
-    },
+    methods: {},
 };
 </script>
