b53c0cc658a7e0a5152156b250645348f6605c76
Ashraful Firoz <ashraf.akon201@gmail.com>
2021-08-03 18:54:44 +0600

    refactored code and removed unused code

--- resources/js/util.js
+++ resources/js/util.js
@@ -22,3 +22,17 @@ export const updatePhotoDetails = (data) => {
             notify("Something went Wrong", "danger");
         });
 }
+
+/**
+ * Helper of `addEventListener`.
+ * @param elem{HTMLElement} - element to bind event
+ * @param type{String} - event type
+ * @param listener{Function} - event listener
+ * @param options{Boolean|Object} - optional parameter which passed as a third parameter to native `addEventListener`
+ * @returns {function(): *} - the unbind callback
+ */
+export function addEventListener(elem, type, listener, options) {
+    elem.addEventListener(type, listener, options);
+
+    return () => elem.removeEventListener(type, listener, options);
+}
