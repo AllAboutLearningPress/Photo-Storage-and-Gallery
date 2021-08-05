f6c306a3c780d2bab4f1f6f50e8b9dba845d3325
Ashraful Firoz <ashraf.akon201@gmail.com>
2021-08-05 15:27:36 +0600

    splitted main app.js file and added code to generate different public.js file for public sharable urls

--- webpack.mix.js
+++ webpack.mix.js
@@ -10,20 +10,6 @@ const mix = require('laravel-mix');
  | file for the application as well as bundling up all the JS files.
  |
  */
-
-mix.js('resources/js/app.js', 'public/js').vue()
-
-    //.copy('resources/js/vendor/pig.min.js', 'public/js/vendor/pig.min.js')
-    .js('resources/js/vendor/pig.js', 'public/js/vendor/pig.min.js')
-    .js('resources/js/frontend/index.js', 'public/js/frontend/bundle.js')
-    .postCss('resources/css/app.css', 'public/css', [
-        require('postcss-import'),
-        // require('tailwindcss'),
-    ])
-    .sass('resources/sass/main.scss', 'public/css/bundle.css')
-    .webpackConfig(require('./webpack.config'));
-
-mix.js('resources/js/public.js', 'public/js/public.js').vue().webpackConfig(require('./webpack.config'));
-if (mix.inProduction()) {
-    mix.version();
+if (process.env.section) {
+    require(`${__dirname}/webpack.mix.${process.env.section}.js`);
 }
