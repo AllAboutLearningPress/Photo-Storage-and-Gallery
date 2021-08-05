61d9f7ccf134102fb9ae604775cb8a44cfe37c42
Ashraful Firoz <ashraf.akon201@gmail.com>
2021-08-05 15:27:36 +0600

    splitted main app.js file and added code to generate different public.js file for public sharable urls

---
+++ webpack.mix.private.js
@@ -0,0 +1,27 @@
+const mix = require('laravel-mix');
+
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel applications. By default, we are compiling the CSS
+ | file for the application as well as bundling up all the JS files.
+ |
+ */
+
+mix.js('resources/js/app.js', 'public/js').vue()
+    .js('resources/js/vendor/pig.js', 'public/js/vendor/pig.min.js')
+    .js('resources/js/frontend/index.js', 'public/js/frontend/bundle.js')
+    .postCss('resources/css/app.css', 'public/css', [
+        require('postcss-import'),
+        // require('tailwindcss'),
+    ])
+    .sass('resources/sass/main.scss', 'public/css/bundle.css')
+    .webpackConfig(require('./webpack.config')).extract(['vue']);
+
+
+if (mix.inProduction()) {
+    mix.version();
+}
