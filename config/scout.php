bfb1322b1256bbe5bd595d364936b6eccbd93bb9
Ashraful Firoz <ashraf.akon201@gmail.com>
2021-08-26 07:52:31 +0600

    minor update in  scout.php config

--- config/scout.php
+++ config/scout.php
@@ -146,7 +146,7 @@ return [
             //     'pass'   => env('ELASTICSEARCH_PASS', 'password'),
             // ]
         ],
-        'port' => env('ELASTICSEARCH_PORT', '9200')
+        // 'port' => env('ELASTICSEARCH_PORT', '9200')
     ]
 
 ];
