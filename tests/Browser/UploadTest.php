be69af45576080bee5fa04bce0a17b1cf184e909
Ashraful Firoz <ashraf.akon201@gmail.com>
2021-09-09 15:34:45 +0200

    Added test for file upload feature

--- tests/Browser/UploadTest.php
+++ tests/Browser/UploadTest.php
@@ -2,33 +2,50 @@
 
 namespace Tests\Browser;
 
+use App\Models\Photo;
 use App\Models\User;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Laravel\Dusk\Browser;
 use Tests\DuskTestCase;
 
+
 class UploadTest extends DuskTestCase
 {
     use DatabaseMigrations;
     /**
-     * A Dusk test example.
+     * Testing the photo upload feature
      *
      * @return void
      */
-    public function testExample()
+    public function testPhotoUpload()
     {
-        $this->artisan('db:seed --class="UserSeeder"');
+        $user = User::factory()->create([
+            'email' => 'testing_user@example.com'
+        ]);
         $this->browse(function (Browser $browser) {
+            $upload_input_selector = '.js-upload__input';
             $browser->loginAs(User::find(1))
                 ->visit('/')
-                ->attach('.js-upload__input', __DIR__ . 'photos/city.jpeg')
-                ->waitForText('Complete upload', 120)
-                ->press(".btn-outline-succes")
-                ->waitFor("#pig")
-                ->pause(10000);
+                ->assertVisible('.header__action-btn__txt') // Upload button
+                ->assertPresent($upload_input_selector) // upload input element
+                ->attach($upload_input_selector, __DIR__ . '/photos/city.jpeg') // selecting the file to input element
+                ->waitForText('Complete upload', 120) // text appears when upload is finished
+                ->assertSee('Complete upload') // checking if text visible
+                ->assertVisible(".btn-outline-success") // check if Complete Upload button is visible
+                ->press(".btn-outline-success") // pressing the button. It will redirect to "/" (home)
+                ->waitFor('.pig-loaded', 5) // class is available when image is loaded
+                ->click('.pig-figure') // opening the first image
+                ->waitFor('.image-view__pic') // this class is present in PhotoView (preview image wrapper div class)
+                ->assertVisible('.image-view__pic');
 
-
-            // code to delete the uploaded file
+            $uploadedPhoto = Photo::where('title', 'city.jpeg')->latest()->first();
+            static::assertNotNull($uploadedPhoto); // checking if photo is entry is stored on DB
+            $browser->visit(route(
+                'photo.show',
+                ['id' => $uploadedPhoto->id, 'slug' => $uploadedPhoto->slug]
+            )) // visiting the PhotoView
+                ->waitFor("img[src*='{$uploadedPhoto->file_name}']") // checking if element is loaded
+                ->assertVisible("img[src*='{$uploadedPhoto->file_name}']"); // checking if photo is visible
 
         });
     }
